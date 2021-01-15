8ad7d71e6a2098b59ede4453ee8661b6fc6edf4e
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-01-14 17:39:55 -0700

    Updated

--- codecrack-javascript/src/hooks/useRandom.js
+++
@@ -1,9 +0,0 @@
-import useLocalStorage from './useLocalStorage';
-
-const useRandom = (initialValues) => {
-    const [randomNum, setRandomNum] = useLocalStorage(initialValues);
-
-    return ([randomNum, setRandomNum])
-};
-
-export default useRandom;
